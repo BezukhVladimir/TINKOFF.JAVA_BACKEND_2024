ec2c104798b1857bdabe5e7af325a65d44ddf60b
Bezukh <91319870+BezukhVladimir@users.noreply.github.com>
2024-02-21 17:29:36 +0500

    refactor: converted to record

--- scrapper/src/main/java/edu/java/dto/github/Response.java
+++ scrapper/src/main/java/edu/java/dto/github/Response.java
@@ -5,39 +5,24 @@ package edu.java.dto.github;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.OffsetDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Response {
-    private Long id;
-    private String type;
-    private Actor actor;
-    private Repo repo;
-
+public record Response(
+    Long id,
+    String type,
+    Actor actor,
+    Repo repo,
     @JsonProperty("created_at")
-    private OffsetDateTime createdAt;
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
+    OffsetDateTime createdAt
+) {
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Actor {
+    public record Actor(
         @JsonProperty("display_login")
-        private String displayLogin;
-    }
+        String displayLogin
+    ) {}
 
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Repo {
-        @JsonProperty("name")
-        private String name;
-    }
+    public record Repo(
+        String name
+    ) {}
 }
--- scrapper/src/main/java/edu/java/dto/stackoverflow/Response.java
+++ scrapper/src/main/java/edu/java/dto/stackoverflow/Response.java
@@ -6,33 +6,20 @@ package edu.java.dto.stackoverflow;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.OffsetDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Response {
-    private Owner owner;
-
+public record Response(
+    Owner owner,
     @JsonProperty("last_activity_date")
-    private OffsetDateTime lastActivityDate;
-
+    OffsetDateTime lastActivityDate,
     @JsonProperty("answer_id")
-    private Long answerId;
-
+    Long answerId,
     @JsonProperty("question_id")
-    private Long questionId;
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
+    Long questionId
+) {
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Owner {
+    public record Owner(
         @JsonProperty("display_name")
-        private String displayName;
-    }
+        String displayName
+    ) {}
 }
