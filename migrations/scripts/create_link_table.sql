19fa8b5844d4cfb099ca7aad0201a52bc2829b74
Bezukh <91319870+BezukhVladimir@users.noreply.github.com>
2024-03-11 01:16:33 +0500

    fix: unique url

--- migrations/scripts/create_link_table.sql
+++ migrations/scripts/create_link_table.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS link_tracker_db.Link (
     id          BIGINT       NOT NULL,
-    url         VARCHAR(255) NOT NULL,
+    url         VARCHAR(255) NOT NULL UNIQUE,
     last_update TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (id)
