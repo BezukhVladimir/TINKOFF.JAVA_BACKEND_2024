6107a59ed9a1f5016d56b6f434bf0d7aed32ae9a
Bezukh <91319870+BezukhVladimir@users.noreply.github.com>
2024-03-25 06:21:50 +0500

    feat: added entities

--- scrapper/src/main/java/edu/java/scrapper/models/Chat.java
+++ scrapper/src/main/java/edu/java/scrapper/models/Chat.java
@@ -1,9 +1,37 @@
 package edu.java.scrapper.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
+import java.util.List;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
-public record Chat(
-    Long id,
-    OffsetDateTime createdAt
-) {
+@Entity
+@Data
+@Accessors(chain = true)
+@Table(schema = "link_tracker_db")
+public class Chat {
+    @Id
+    @NotNull
+    private Long id;
+
+    @Column(name = "created_at")
+    @NotNull
+    private OffsetDateTime createdAt;
+
+    @ManyToMany
+    @JoinTable(
+        name = "chats_links",
+        joinColumns =        @JoinColumn(name = "id_chat"),
+        inverseJoinColumns = @JoinColumn(name = "id_link")
+    )
+
+    private List<Link> links;
 }
--- scrapper/src/main/java/edu/java/scrapper/models/Link.java
+++ scrapper/src/main/java/edu/java/scrapper/models/Link.java
@@ -1,11 +1,49 @@
 package edu.java.scrapper.models;
 
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Converter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import java.net.URI;
 import java.time.OffsetDateTime;
+import java.util.List;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
-public record Link(
-    Long id,
-    URI url,
-    OffsetDateTime lastUpdate
-) {
+@Entity
+@Data
+@Accessors(chain = true)
+@Table(schema = "link_tracker_db")
+public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Convert(converter = URIConverter.class)
+    private URI url;
+
+    @Column(name = "last_update")
+    private OffsetDateTime lastUpdate;
+
+    @ManyToMany(mappedBy = "links")
+    private List<Chat> chats;
+
+    @Converter(autoApply = true)
+    static class URIConverter implements AttributeConverter<URI, String> {
+        @Override
+        public String convertToDatabaseColumn(URI attribute) {
+            return attribute.toString();
+        }
+
+        @Override
+        public URI convertToEntityAttribute(String dbData) {
+            return URI.create(dbData);
+        }
+    }
 }
