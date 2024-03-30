912defcb248f6331b7b39869a37da4a049e675b1
Bezukh <91319870+BezukhVladimir@users.noreply.github.com>
2024-03-30 07:17:10 +0500

    feat: added ip-filter

---
+++ scrapper/src/main/java/edu/java/scrapper/filters/TooManyRequests429Filter.java
@@ -0,0 +1,35 @@
+package edu.java.scrapper.filters;
+
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.ConsumptionProbe;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+@Component
+@RequiredArgsConstructor
+public class TooManyRequests429Filter extends OncePerRequestFilter {
+    private final Bucket bucket;
+
+    @Override
+    protected void doFilterInternal(
+        @NotNull HttpServletRequest request,
+        @NotNull HttpServletResponse response,
+        @NotNull FilterChain filterChain
+    ) throws IOException, ServletException {
+        ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
+
+        if (probe.isConsumed()) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(429);
+        }
+    }
+}
